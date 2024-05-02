87ecc86d1923051228c694e41cb055f2ba6b8a87
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-05-02 09:47:19 -0600

    Inferrir tipos y modo estricto

--- bases/app.js
+++ bases/app.js
@@ -1,3 +1,13 @@
 "use strict";
-const msg = 'hola mundo';
-console.log(msg);
+// const msg: string = 'hola mundo';
+// const hero = {
+//     name: 'Iroman',
+//     age: 45
+// }
+// hero.age = 50;
+// console.log(hero.age + 1);
+// funcion anonima autoinvocada, per,ite crear varios archivos en el mismo projecto y de esa manera no cae dentro del scope global y no marca error
+(() => {
+    const a = 10;
+    console.log(a);
+})();
