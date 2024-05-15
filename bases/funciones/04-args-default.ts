9d2c5cc239225ed20b9be15bf582f7d26974c8aa
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-05-15 08:43:48 -0600

    Parametros opcionales y por defecto

---
+++ bases/funciones/04-args-default.ts
@@ -0,0 +1,13 @@
+(() => {
+    const fullName = (firstName: string, lastName?: string, upper: boolean = false): string => {
+        if (upper) {
+            return `${firstName} ${lastName || 'no lastname'}`.toUpperCase();
+        } else {
+            return `${firstName} ${lastName || 'no lastname'}`;
+        }
+    }
+
+    const name = fullName('Edwin', 'Alexander', true);
+    console.log(name);
+    
+})()
