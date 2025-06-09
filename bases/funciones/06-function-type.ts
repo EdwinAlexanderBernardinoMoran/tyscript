d161b2645b1f33d2bb2ae7d82832a3cbb24a6123
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-06-09 14:04:35 -0600

    S4 - Tipo Funcion v2

--- bases/funciones/06-function-type.ts
+++ bases/funciones/06-function-type.ts
@@ -1,4 +1,22 @@
 (() => {
-    const addNumber = (a: number, b: number) => a + b;
+    const addNumbers = (a: number, b: number) => a + b;
     const greet = (name: string) => `Hello, ${ name }`;
+    const saveTheWorld = () => 'The world has been saved!';
+
+    let myFunction: Function;
+    // let myFunction: (a: number, b: number) => number;
+    // let myFunction: ( a: string) => string;
+    // let myFunction: () => string;
+
+    // myFunction = 10;
+    // console.log(myFunction);
+
+    myFunction = addNumbers;
+    console.log(myFunction(1, 2));
+
+    myFunction = greet;
+    console.log(myFunction("Juan"));
+
+    myFunction = saveTheWorld;
+    console.log(myFunction());
 })()
